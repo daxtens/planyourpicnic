3922339ae5340e1fa59cfe1117425b6451e3ccf2
Andrew Donnellan <andrew@donnellan.id.au>
2013-06-01 09:04:01 +1000

    Added SimpleKML to fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,8 @@ from fabric.api import *
 def setup():
 	local("pip install bottle")
 	local("pip install psycopg2")
-
+	local("pip install simplekml")
+	
         # syntax checkers and cleaners
         local("pip install pep8")
         local("easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz")
