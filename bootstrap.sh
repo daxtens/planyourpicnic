4178d62348be1f3bc7d56d7839537084fa6a03e6
Daniel Axtens <daniel@axtens.net>
2013-06-01 09:58:46 +1000

    Use fabric from ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,12 @@
 
 sudo apt-get update
 
-sudo apt-get install -y python-virtualenv python-dev postgresql libpq-dev git
+sudo apt-get install -y python-virtualenv python-dev postgresql libpq-dev git fabric
 
 cd /vagrant
 rm -rf bin/ lib/ include/
 virtualenv .
 source bin/activate
-pip install fabric
 fab setup
 
 echo "You're good to go: vagrant ssh, cd /vagrant, source bin/activate"
