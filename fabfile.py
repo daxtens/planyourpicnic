081eaefefbbdb42071928bafadb678c4a35f5d47
Daniel Axtens <daniel@axtens.net>
2013-06-01 08:30:29 +1000

    Fabfile for gjslint and pep8

--- fabfile.py
+++ fabfile.py
@@ -2,3 +2,7 @@ from fabric.api import *
 
 def setup():
 	local("pip install bottle")
+
+        # syntax checkers and cleaners
+        local("pip install pep8")
+        local("easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz")
