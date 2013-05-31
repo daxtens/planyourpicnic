8527346829d9707f448e10c9bbc63476b100e42b
Daniel Axtens <daniel@axtens.net>
2013-06-01 09:47:12 +1000

    Vagrant!

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y python-virtualenv python-dev postgresql libpq-dev git
+
+cd /vagrant
+rm -r bin/ lib/ include/
+virtualenv .
+source bin/activate
+pip install fabric
 fab setup
+
+echo "You're good to go: vagrant ssh, cd /vagrant, source bin/activate"
