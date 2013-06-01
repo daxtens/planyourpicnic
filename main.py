729f70ad813d88ba0acb685e92214bfa5178ba38
Daniel Axtens <daniel@axtens.net>
2013-06-01 13:16:32 +1000

    Make things play nice with supervisord.

--- main.py
+++ main.py
@@ -26,5 +26,9 @@ def static(path):
     return bottle.static_file(path, root=os.path.dirname(__file__))
 
 if __name__=="__main__":
+    # get things in order for when we're lanuched from supervisor
+    os.chdir(os.path.dirname(__file__))
+    sys.path.append(os.path.dirname(__file__))
+    # run it
     bottle.run(reloader=True, debug=True, host="0.0.0.0")
 
