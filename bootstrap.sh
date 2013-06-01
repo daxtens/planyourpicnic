e179ec4a55054501683bdfb4458f351f747a8388
Andrew Donnellan <andrew@donnellan.id.au>
2013-06-01 13:25:30 +1000

    Bootstrap script *should* now create a database and user

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,13 @@ sudo apt-get update
 sudo apt-get install -y python-virtualenv python-dev postgresql-9.1 postgresql-client-9.1 libpq-dev git fabric
 
 cd /vagrant
+
+sudo -u postgres psql << EOF
+CREATE USER planyourpicnic WITH PASSWORD 'planyourpicnic';
+CREATE DATABASE planyourpicnic;
+GRANT ALL PRIVILEGES ON DATABASE planyourpicnic TO planyourpicnic;
+EOF
+
 fab setup
 
 echo "You're good to go: vagrant ssh, cd /vagrant, python main.py"
