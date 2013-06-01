aa6cd34f6e53497b68321d34bd5e7a5383e4df94
Andrew Donnellan <andrew@donnellan.id.au>
2013-06-01 14:08:36 +1000

    Fix typo in init sql (this will be reflected shortly in generator script)

--- data/init.sql
+++ data/init.sql
@@ -1,6 +1,6 @@
 
 
-CREATE USER planyourpicnic WITH PASSWORD planyourpicnic;
+CREATE USER planyourpicnic WITH PASSWORD 'planyourpicnic';
 CREATE DATABASE planyourpicnic;
 GRANT ALL PRIVILEGES ON DATABASE planyourpicnic TO planyourpicnic;
 
