1046764459ff00df2b1ddb66622ad45ff133aad8
Daniel Axtens <daniel@axtens.net>
2013-06-01 13:19:08 +1000

    Make the last change play nice with vagrant.

--- main.py
+++ main.py
@@ -3,16 +3,19 @@
 import bottle
 import os
 
+
 @bottle.route('/')
 @bottle.route('/index.html')
 @bottle.view('index')
 def index():
     return {'name': 'GovHack Project'}
 
+
 @bottle.route('/about')
 @bottle.view('about')
 def about():
     return {}
+
 
 @bottle.route('/contact')
 @bottle.view('contact')
@@ -25,10 +28,11 @@ def static(path):
     print(os.path.dirname(__file__))
     return bottle.static_file(path, root=os.path.dirname(__file__))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # get things in order for when we're lanuched from supervisor
-    os.chdir(os.path.dirname(__file__))
-    sys.path.append(os.path.dirname(__file__))
+    if os.path.dirname(__file__):
+        os.chdir(os.path.dirname(__file__))
+        sys.path.append(os.path.dirname(__file__))
+
     # run it
     bottle.run(reloader=True, debug=True, host="0.0.0.0")
-
