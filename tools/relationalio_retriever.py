ad9eee2d0b3aac565d602746725ae61b1aadeb60
jonoerik <jonoerik@fastmail.fm>
2013-06-01 15:37:43 +1000

    minor relationalio retriever updates

--- tools/relationalio_retriever.py
+++ tools/relationalio_retriever.py
@@ -1,21 +1,36 @@
 # Download data from relational.io, and load into local PostgreSQL database
+# Doesn't work; don't use
 
 import psycopg2
+import psycopg2.extras
 import getpass
-import ../settings.py
+import sys
+sys.path.append('..')
+from settings import *
 
-#TODO should these be stored or requested each time?
+# TODO should these be stored or requested each time?
 rio_user = raw_input("RelationalIO Username: ")
 rio_pw = getpass.getpass("RelationalIO Password: ")
-rio_conn = psycopg2.connect(host="workspace.govhack.relational.io", database=rio_user, user=rio_user, password=rio_pw, port=5432, sslmode="require")
-rio_cur = rio_conn.cursor()
+rio_conn = psycopg2.connect(host="workspace.govhack.relational.io",
+                            database=rio_user, user=rio_user, password=rio_pw,
+                            port=5432, sslmode="require")
+rio_cur = rio_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-local_user = "" #TODO get local server details
-local_pw = "" #TODO (can this be stored safely?
-local_conn = psycopg2.connect(host=DB_HOST, database=DB_DATABASE, user=DB_USER, password=DB_PASSWORD, port=DB_PORT)
+local_user = "" # TODO get local server details
+local_pw = "" # TODO (can this be stored safely?
+local_conn = psycopg2.connect(host=DB_HOST, database=DB_DATABASE,
+                              user=DB_USER, password=DB_PASSWORD, port=DB_PORT)
 local_cur = local_conn.cursor()
 
-#TODO remove
+rio_cur.execute("SELECT (asset_id,surface_type,location) FROM act_gov_au$playgroundsintheact.playgroundsintheact")
+row = rio_cur.fetchone()
+print row
+print row.keys()
+print row['asset_id']
+#for result in rio_cur:
+#    print result['asset_id']
+
+# TODO remove
 #rio_cur.execute("SELECT * FROM act_gov_au$playgroundsintheact.playgroundsintheact")
 #results = rio_cur.fetchall()
 #for row in results:
