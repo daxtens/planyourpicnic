2f0cddb7f77599bdb777872d9aa099bf9ecac91d
Tessa Bradbury <tessereth@yahoo.com.au>
2013-06-01 10:10:26 +1000

    Windows sucks.

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def index():
     return {'name': 'GovHack Project'}
 
 if __name__=="__main__":
-    bottle.run(reloader=True, debug=True)
+    bottle.run(reloader=True, debug=True, host="0.0.0.0")
 
