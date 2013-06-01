ca27eec0786ba6a7ed295d1062285e6786567be3
Daniel Axtens <daniel@axtens.net>
2013-06-01 10:13:22 +1000

    Don't try to virtualenv on vagrant.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,6 @@ sudo apt-get update
 sudo apt-get install -y python-virtualenv python-dev postgresql libpq-dev git fabric
 
 cd /vagrant
-rm -rf bin/ lib/ include/
-virtualenv .
-source bin/activate
 fab setup
 
-echo "You're good to go: vagrant ssh, cd /vagrant, source bin/activate"
+echo "You're good to go: vagrant ssh, cd /vagrant, python main.py"
